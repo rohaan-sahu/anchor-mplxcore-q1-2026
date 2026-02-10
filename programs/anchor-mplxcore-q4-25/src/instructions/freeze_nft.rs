bea204215e31a240981b292b247092e7d54cba2a
rohaan-sahu <rbbsr8292@gmail.com>
2026-02-10 13:46:48 +0000

    All instructions done

--- programs/anchor-mplxcore-q4-25/src/instructions/freeze_nft.rs
+++ programs/anchor-mplxcore-q4-25/src/instructions/freeze_nft.rs
@@ -7,14 +7,72 @@ use mpl_core::{
 
 use crate::{error::MPLXCoreError, state::CollectionAuthority};
 
-// #[derive(Accounts)]
-// pub struct FreezeNft<'info> {
-//    // TODO
-// }
+#[derive(Accounts)]
+pub struct FreezeNft<'info> {
+    // CHECK: This will also be checked by core
+    #[account(address = CORE_PROGRAM_ID)]
+    pub core_program: UncheckedAccount<'info>,
+    pub system_program: Program<'info, System>,
 
-// impl<'info> FreezeNft<'info> {
-//     pub fn freeze_nft(&mut self) -> Result<()> {
-//         // TODO
-//         Ok(())
-//     }
-// }
+   #[account(mut)]
+    pub minter: Signer<'info>,
+
+    #[account(mut, constraint = asset.data_is_empty() @ MPLXCoreError::AssetAlreadyInitialized)]
+    pub asset: Signer<'info>,
+
+    #[account(
+        mut,
+        constraint = collection.owner == &CORE_PROGRAM_ID @ MPLXCoreError::InvalidCollection,
+        constraint = !collection.data_is_empty() @ MPLXCoreError::CollectionNotInitialized
+    )]
+    /// CHECK: This will also be checked by core
+    pub collection: UncheckedAccount<'info>,
+
+    #[account(
+        seeds = [b"collection_authority", collection.key().as_ref()],
+        bump = collection_authority.bump,
+    )]
+    pub collection_authority: Account<'info, CollectionAuthority>,
+
+    
+    
+}
+
+impl<'info> FreezeNft<'info> {
+    pub fn freeze_nft(&mut self) -> Result<()> {
+
+        let (collection_authority_pda, _collection_authority_pda_bump) = Pubkey::find_program_address(
+            &[b"collection_authority",self.collection.key.as_ref()],
+            &crate::ID
+        );
+
+        require_keys_eq!(
+            collection_authority_pda,
+            self.collection_authority.key(),
+            MPLXCoreError::InvalidCollection
+        );
+        require_keys_eq!(
+            self.collection_authority.creator,
+            self.minter.key(),
+            MPLXCoreError::NotAuthorized
+        );
+
+        let binding = self.collection.key();
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            b"collection_authority".as_ref(),
+            binding.as_ref(),
+            &[self.collection_authority.bump],
+        ]];
+
+        UpdatePluginV1CpiBuilder::new(&self.core_program)
+            .asset(&self.asset)
+            .collection(Some(&self.collection))
+            .payer(&self.minter.to_account_info())
+            .authority(Some(&self.collection_authority.to_account_info()))
+            .system_program(&self.system_program.to_account_info())
+            .plugin(Plugin::FreezeDelegate(FreezeDelegate {frozen: true}))
+            .invoke_signed(signer_seeds)?;
+
+        Ok(())
+    }
+}
--- programs/anchor-mplxcore-q4-25/src/instructions/update_nft.rs
+++ programs/anchor-mplxcore-q4-25/src/instructions/update_nft.rs
@@ -1,1 +1,78 @@
-// TODO
+use anchor_lang::prelude::*;
+use mpl_core::{
+    instructions::UpdateV2CpiBuilder,
+    ID as CORE_PROGRAM_ID,
+};
+
+use crate::{error::MPLXCoreError, state::CollectionAuthority};
+
+#[derive(Accounts)]
+pub struct UpdateNft<'info> {
+    // CHECK: This will also be checked by core
+    #[account(address = CORE_PROGRAM_ID)]
+    pub core_program: UncheckedAccount<'info>,
+    pub system_program: Program<'info, System>,
+
+   #[account(mut)]
+    pub minter: Signer<'info>,
+
+    #[account(mut, constraint = asset.data_is_empty() @ MPLXCoreError::AssetAlreadyInitialized)]
+    pub asset: Signer<'info>,
+
+    #[account(
+        mut,
+        constraint = collection.owner == &CORE_PROGRAM_ID @ MPLXCoreError::InvalidCollection,
+        constraint = !collection.data_is_empty() @ MPLXCoreError::CollectionNotInitialized
+    )]
+    /// CHECK: This will also be checked by core
+    pub collection: UncheckedAccount<'info>,
+
+    #[account(
+        seeds = [b"collection_authority", collection.key().as_ref()],
+        bump = collection_authority.bump,
+    )]
+    pub collection_authority: Account<'info, CollectionAuthority>,
+
+    
+    
+}
+
+impl<'info> UpdateNft<'info> {
+    pub fn update_nft(&mut self, name: String) -> Result<()> {
+
+        let (collection_authority_pda, _collection_authority_pda_bump) = Pubkey::find_program_address(
+            &[b"collection_authority",self.collection.key.as_ref()],
+            &crate::ID
+        );
+
+        require_keys_eq!(
+            collection_authority_pda,
+            self.collection_authority.key(),
+            MPLXCoreError::InvalidCollection
+        );
+
+        require_keys_eq!(
+            self.collection_authority.creator,
+            self.minter.key(),
+            MPLXCoreError::NotAuthorized
+        );
+
+        let binding = self.collection.key();
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            b"collection_authority".as_ref(),
+            binding.as_ref(),
+            &[self.collection_authority.bump],
+        ]];
+
+        UpdateV2CpiBuilder::new(&self.core_program)
+            .asset(&self.asset)
+            .collection(Some(&self.collection))
+            .payer(&self.minter.to_account_info())
+            .authority(Some(&self.collection_authority.to_account_info()))
+            .system_program(&self.system_program.to_account_info())
+            .new_name(name)
+            .invoke_signed(signer_seeds)?;
+        
+        Ok(())
+    }
+}
